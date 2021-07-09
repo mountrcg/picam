e5d7edebfff43a4336d0136e411b63caa9a34e10
mountrcg <comps@diprobe.com>
2021-07-09 11:27:29 +0200

    show seconds to sunset take pic until 35min after sunset

--- README.md
+++
@@ -1,1 +0,0 @@
-This is the software for a Raspberry Pi based time lapse camera. For more information, see: http://www.makethenmakeinstall.com/2017/05/picam-raspberry-pi-battery-powered-timelapse-camera-part-1/
--- takePic.sh
+++ takePic.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-OUTPUTDIR="~/images"
-SCRIPTSDIR="~/picam"
+OUTPUTDIR="/images"
+SCRIPTSDIR="/root/picam"
 DRIVEUUID="7309-DD8D"
 
-LOGFILE="$OUTPUTDIR/images.log"
+LOGFILE="$SCRIPTSDIR/images.log"
 
 SLEEPBETWEENPICS=300  ## note: delay will actually be this value plus the time it takes for the pi to boot and shutdown
 INTERVAL=4 ## note: time between pictures in hours
-RESTART=`date -d "+$INTERVAL hour" -Iseconds`. ## time of the day to reboot after taking pic, based on interval
+RESTART=`date -d "+$INTERVAL hour" -Iseconds` ## time of the day to reboot after taking pic, based on interval
 
 # latitude and longitude for your location
 LAT="52.5811N"
@@ -52,8 +52,8 @@ TOMORROW_RISE_ISO=`date -d"$TOMORROW_RISE" +"%Y-%m-%dT%H:%M:%S%:z"`
 # time between now and sunset
 SUNSETDIFF=`$SCRIPTSDIR/timediff.sh "$NOW" "$SETTIME"`
 
-# if we'are less than the set hours before sunset+1hour, don't take another picture and sleep until sunrise
-if [ $SUNSETDIFF -lt 3600*$INTERVALL ];
+# if we'are more than 35 min after sunset, don't take another picture and sleep until sunrise
+if [ $SUNSETDIFF -lt -1805 ]; # if 35min after sunset still take a picture
 then
 	# next picture should be after sunrise
 	NOWTOMIDNIGHT=`$SCRIPTSDIR/timediff.sh "$NOW" "23:59:59"`
@@ -68,10 +68,15 @@ else
     raspistill -o "$OUTPUTDIR/image-$DATE.jpg" -ts --exif -ex=auto
 fi
 
+if [ $SUNSETDIFF -lt 12600 ];
+    then RESTART=$TOMORROW_RISE_ISO # restart next day, if only less than 3.5hrs till sunset
+fi
+
 # write to log
-echo "`date` - $ACTION sleep until: $RESTART" >> $LOGFILE
+echo "`date -Iminutes` - $ACTION, $SUNSETDIFF secs to sunset, sleep until: $RESTART" >> $LOGFILE
 
 # next power up
+# RESTART=`date -d "+$INTERVAL minute" -Iseconds` #for restart in 4min
 echo "rtc_alarm_set $RESTART 127" | nc -q 0 127.0.0.1 8423
 # shutdown
-sudo shutdown now
+sudo shutdown -h 2
